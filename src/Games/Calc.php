f761250544df17abe5695d45dd95cf30dacec1ea
Николай Рябинин <rnn2002@mail.ru>
2023-06-30 13:58:36 +0300

    Refactoring Games/Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -2,24 +2,13 @@
 
 namespace BrainGames\Games\Calc;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Routine\gameRoutine;
 
 const GOAL = 'What is the result of the expression?';
 
-const MAX_NUMBER_OF_QUESTIONS = 3;
-const QUESTION_PHRASE = 'Question: ';
-const ANSWER_PHRASE = 'Your answer';
-const SUCCESS_GAME = 'Congratulations, ';
-const SUCCESS_QUESTION = 'Correct!';
-const FAIL_GAME = 'Let\'s try again, ';
-const FAIL_QUESTION = ' is wrong answer ;(. Correct answer was ';
-
-function game($name)
+function game()
 {
-    line(GOAL);
-
-    for ($i = 1; $i <= MAX_NUMBER_OF_QUESTIONS; $i += 1) {
+    $gameRandomizer = function () {
         $firstOperand = rand(1, 30);
         $secondOperand = rand(1, 30);
         $operation = match (rand(1, 3)) {
@@ -32,16 +21,10 @@ function game($name)
             '-' => $firstOperand - $secondOperand,
             '*' => $firstOperand * $secondOperand,
         };
+        $expression = implode(' ', [$firstOperand, $operation, $secondOperand]);
 
-        line('%s%s %s %s', QUESTION_PHRASE, $firstOperand, $operation, $secondOperand);
-        $answer = strtolower(trim(prompt(ANSWER_PHRASE)));
-        if ($answer != $numberToGuess) {
-            line('\'%s\'%s\'%s\'.', $answer, FAIL_QUESTION, $numberToGuess);
-            line('%s%s!', FAIL_GAME, $name);
-            exit();
-        }
-        line(SUCCESS_QUESTION);
-    }
-    line('%s%s!', SUCCESS_GAME, $name);
-    exit;
+        return [$expression, $numberToGuess];
+    };
+
+    gameRoutine(GOAL, $gameRandomizer);
 }
