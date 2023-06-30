ba30f8116567c396495a30e8daad2ceb282bca5e
Николай Рябинин <rnn2002@mail.ru>
2023-06-30 13:49:48 +0300

    Refactoring Games/Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -2,22 +2,12 @@
 
 namespace BrainGames\Games\Prime;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Routine\gameRoutine;
 
 const GOAL = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const MAX_NUMBER_OF_QUESTIONS = 3;
-const QUESTION_PHRASE = 'Question: ';
-const ANSWER_PHRASE = 'Your answer';
-const SUCCESS_GAME = 'Congratulations, ';
-const SUCCESS_QUESTION = 'Correct!';
-const FAIL_GAME = 'Let\'s try again, ';
-const FAIL_QUESTION = ' is wrong answer ;(. Correct answer was ';
 
-function game($name)
+function game()
 {
-    line(GOAL);
-
     $primeNumbers = [];
 
     for ($i = 2; $i < 100; $i++) {
@@ -32,18 +22,11 @@ function game($name)
         }
     }
 
-    for ($i = 1; $i <= MAX_NUMBER_OF_QUESTIONS; $i += 1) {
+    $gameRandomizer = function () use ($primeNumbers) {
         $numberToGuess = rand(2, 99);
         $isPrime = (in_array($numberToGuess, $primeNumbers)) ? 'yes' : 'no';
-        line('%s%s', QUESTION_PHRASE, $numberToGuess);
-        $answer = strtolower(trim(prompt(ANSWER_PHRASE)));
-        if ($answer !== $isPrime) {
-            line('\'%s\'%s\'%s\'.', $answer, FAIL_QUESTION, $isPrime);
-            line('%s%s!', FAIL_GAME, $name);
-            exit();
-        }
-        line(SUCCESS_QUESTION);
-    }
-    line('%s%s!', SUCCESS_GAME, $name);
-    exit;
+        return [$numberToGuess, $isPrime];
+    };
+
+    gameRoutine(GOAL, $gameRandomizer);
 }
